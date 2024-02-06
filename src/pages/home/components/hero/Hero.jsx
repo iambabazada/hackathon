ffb3434ae7e88355c12468be513e88291499732a
iambabazada <iambabazada@gmail.com>
2024-02-07 01:37:10 +0400

    accepted changes

--- src/pages/home/components/hero/Hero.jsx
+++ src/pages/home/components/hero/Hero.jsx
@@ -1,32 +1,29 @@
-import React from 'react'
-import styles from './hero.module.css'
-import HeroImg from '../../../../assets/images/hero_img.png'
-import Button from '../../../../components/button/Button'
+import React from 'react';
+import styles from './hero.module.css';
+import HeroImg from '../../../../assets/images/hero_img.png';
+import Button from '../../../../components/button/Button';
 
 const Hero = () => {
     return (
         <div className={styles.hero}>
-            <div className={styles.left}>
+            <div className={`${styles.left} animate__fadeInDownBig`}>
                 <h2>
-                    Vos travaux moins chers grâce aux aides financières
+                    E-commercional Startup
                 </h2>
-                    <p>
-                        Nous vous accompagnons tout au long de votre démarche dé rénovation thermique et de transition énergétique sans prise de tête.
-                    </p>
-                    <div className={styles.btns}>
-                        <Button width='lg'>
-                            Ağac al
-                        </Button>
-                        <Button width='lg'>
-                            Ərazi seç
-                        </Button>
-                    </div>
+                <div className={styles.btns}>
+                    <Button width='lg'>
+                        Ağac al
+                    </Button>
+                    <Button width='lg'>
+                        Ərazi seç
+                    </Button>
+                </div>
             </div>
             <div className={styles.right}>
                 <img src={HeroImg} alt="" />
             </div>
         </div>
-    )
+    );
 }
 
-export default Hero
+export default Hero;
